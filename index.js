78962943f9d6efb3793faba419558f5eb70b5581
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2019-10-30 01:47:14 +0000

    Update index file with exports from utils

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from './utilities/background';
+export * from './utilities/border';
+export * from './utilities/display';
+export * from './utilities/flex';
+export * from './utilities/fontSize';
+export * from './utilities/padding';
+export * from './utilities/position';
