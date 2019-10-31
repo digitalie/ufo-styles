0be959fab7583c6a3f436b85ffc17dfc565c4def
Keith Wright <keitwright@deloitte.ie>
2019-10-31 10:21:50 +0000

    add some useful utilities to size

--- src/utilities/size.js
+++ src/utilities/size.js
@@ -32,3 +32,16 @@ export const size = (widthValue, heightValue) => {
 export const minimumSize = (widthValue, heightValue) => {
     return compose(minimumWidth(widthValue), minimumHeight(heightValue));
 };
+
+export const size100 = size("100%", "100%");
+export const size50 = size("50%", "50%");
+export const size33 = size("33.333333%", "33.333333%");
+export const size25 = size("25%", "25%");
+export const width100 = width("100%");
+export const width50 = width("50%");
+export const width33 = width("33.333333%");
+export const width25 = width("25%");
+export const height100 = height("100%");
+export const height50 = height("50%");
+export const height33 = height("33.333333%");
+export const height25 = height("25%");
