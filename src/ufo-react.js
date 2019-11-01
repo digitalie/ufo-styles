8b324f6dca71ebb039e72d5898f0e3daa8e6951d
Keith Wright <keitwright@deloitte.ie>
2019-11-01 16:53:56 +0000

    change the react hook to accept multiple parameters instead of an array

--- src/ufo-react.js
+++ src/ufo-react.js
@@ -5,8 +5,12 @@ export const configure = (config) => {
     ufoDom.configure(config);
 }
 
-export const useStylesAsClassName = (cssInJsIObjects, dependencies) => {
+export const useUfoClassName = (...cssInJsIObjects) => {
     const [className, setClassName] = useState('');
+    let dependencies;
+    if(Array.isArray(cssInJsIObjects[cssInJsIObjects.length - 1])){
+        dependencies = cssInJsIObjects.pop();
+    }
 
     useEffect(() => {
         const newClassName = ufoDom.createStyleTag(cssInJsIObjects);
