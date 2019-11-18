b7319d3db20b9329221536d2c6af947fefa4be70
Keith Wright <keitwright@deloitte.ie>
2019-11-18 18:02:02 +0000

    refactor size utilities

--- src/lib/compose.js
+++ src/lib/compose.js
@@ -2,9 +2,9 @@ export const compose = (...objects) => {
     return objects.reduce((target, source) => {
         for (var property in source) {
             if (
-				source.hasOwnProperty(property) &&
-				!property.startsWith("_") &&
-                typeof source[property] !== "function"                
+                source.hasOwnProperty(property) &&
+                !property.startsWith("_") &&
+                typeof source[property] !== "function"
             ) {
                 target[property] = source[property];
             }
--- src/utilities/size.js
+++ src/utilities/size.js
@@ -1,53 +1,53 @@
 import { formatUnitValue } from "../lib/format";
 import { merge } from "../lib/merge";
 
-export const minimumWidth = value => {
-    return {
-        minWidth: formatUnitValue(value)
-    };
+export const size = {
+    minimumWidth: function (value) {
+        return merge(
+            this,
+            {
+                minWidth: formatUnitValue(value)
+            }
+        );
+    },
+    minimumHeight: function (value) {
+        return merge(
+            this,
+            {
+                minHeight: formatUnitValue(value)
+            }
+        );
+    },
+    fixedWidth: function (value) {
+        return merge(
+            this,
+            {
+                width: formatUnitValue(value)
+            }
+        );
+    },
+    fixedHeight: function (value) {
+        return merge(
+            this,
+            {
+                height: formatUnitValue(value)
+            }
+        );
+    },
+    fixedSize: function (widthValue, heightValue) {
+        return this.fixedWidth(widthValue).fixedHeight(heightValue);
+    },
+    minimumSize: function (widthValue, heightValue) {
+        return this.minimumWidth(widthValue).minimumHeight(heightValue);
+    },
+    fullSize: function () {
+        return this.fixedSize("100%", "100%");
+    },
+    fullWidth: function () {
+        return this.fixedWidth("100%");
+    },
+    fullHeight: function () {
+        return this.fixedHeight("100%");
+    },
 };
 
-export const width = value => {
-    return {
-        width: formatUnitValue(value)
-    };
-};
-
-export const minimumHeight = value => {
-    return {
-        minHeight: formatUnitValue(value)
-    };
-};
-
-export const height = value => {
-    return {
-        height: formatUnitValue(value)
-    };
-};
-
-export const size = (widthValue, heightValue) => {
-    return merge(
-        width(widthValue),
-        height(heightValue)
-    );
-};
-
-export const minimumSize = (widthValue, heightValue) => {
-    return merge(
-        minimumWidth(widthValue),
-        minimumHeight(heightValue)
-    );
-};
-
-export const size100 = size("100%", "100%");
-export const size50 = size("50%", "50%");
-export const size33 = size("33.333333%", "33.333333%");
-export const size25 = size("25%", "25%");
-export const width100 = width("100%");
-export const width50 = width("50%");
-export const width33 = width("33.333333%");
-export const width25 = width("25%");
-export const height100 = height("100%");
-export const height50 = height("50%");
-export const height33 = height("33.333333%");
-export const height25 = height("25%");
